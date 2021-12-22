5ce53296b73873f784eca110dd97221cfa3e490a
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2021-12-22 00:09:19 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct body

---
+++ src/domain/test/authentication/mock-authentication.ts
@@ -0,0 +1,8 @@
+import * as faker from 'faker';
+
+import { AuthenticationParams } from '../../usecases/authentication';
+
+export const mockAuthentication = (): AuthenticationParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+});
