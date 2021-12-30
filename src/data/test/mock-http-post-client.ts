d7133df588970b5f5bf69614b89ce2ed7e4bff29
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2021-12-30 19:14:02 -0300

    refactor: add generic params to HttpPostClient

--- src/data/test/mock-http-post-client.ts
+++ src/data/test/mock-http-post-client.ts
@@ -4,16 +4,16 @@ import {
 } from '@/data/protocols/http/http-post-client';
 import { HttpResponse, HttpStatusCode } from '../protocols/http/http-response';
 
-export class HttpPostCLientSpy implements HttpPostClient {
+export class HttpPostCLientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string;
 
-  body?: object;
+  body?: T;
 
-  response: HttpResponse = {
+  response: HttpResponse<R> = {
     statusCode: HttpStatusCode.ok,
   };
 
-  async post(params: HttpPostParams): Promise<HttpResponse> {
+  async post(params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url;
     this.body = params.body;
     return Promise.resolve(this.response);
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -6,14 +6,19 @@ import { mockAuthentication } from '@/domain/test/authentication/mock-authentica
 import { InvalidCredentialsError } from '@/domain/error/invalid-credentials-error';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import { UnexpectedError } from '@/domain/error/unexpected-error';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
+import { AccountModel } from '@/domain/models/account-model';
 
 type SutTypes = {
   sut: RemoteAuthentication;
-  httpPostClientSpy: HttpPostCLientSpy;
+  httpPostClientSpy: HttpPostCLientSpy<AuthenticationParams, AccountModel>;
 };
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostCLientSpy();
+  const httpPostClientSpy = new HttpPostCLientSpy<
+    AuthenticationParams,
+    AccountModel
+  >();
   const sut = new RemoteAuthentication(url, httpPostClientSpy);
   return {
     sut,
