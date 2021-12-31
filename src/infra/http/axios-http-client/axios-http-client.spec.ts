b94095990f049d857dd5b5ec4b81b32f1eb00ec1
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2021-12-31 10:26:22 -0300

    refactor: move SUT axios to factory method in infra layer

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,18 +1,23 @@
-import axios from 'axios';
 import * as faker from 'faker';
+import axios from 'axios';
 import { HttpPostParams } from '@/data/protocols/http';
 import { AxiosHttpClient } from './axios-http-client';
+import { mockAxios } from '@/infra/tests';
 
 jest.mock('axios');
-const mockedAxios = axios as jest.Mocked<typeof axios>;
-const mockedAxiosResult = {
-  data: faker.random.objectElement(),
-  status: faker.datatype.number(),
-};
-mockedAxios.post.mockResolvedValue(mockedAxiosResult);
 
-const makeSut = (): AxiosHttpClient => {
-  return new AxiosHttpClient();
+type SutTypes = {
+  sut: AxiosHttpClient;
+  mockedAxios: jest.Mocked<typeof axios>;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient();
+  const mockedAxios = mockAxios();
+  return {
+    sut,
+    mockedAxios,
+  };
 };
 
 const mockPostRequest = (): HttpPostParams<any> => ({
@@ -23,17 +28,14 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 describe('AxiosHttpClient', () => {
   it('Should call axios with correct values', async () => {
     const request = mockPostRequest();
-    const sut = makeSut();
+    const { sut, mockedAxios } = makeSut();
     await sut.post(request);
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
   });
 
   it('Should return correct statusCode and body', async () => {
-    const sut = makeSut();
-    const httpResponse = await sut.post(mockPostRequest());
-    expect(httpResponse).toEqual({
-      statusCode: mockedAxiosResult.status,
-      body: mockedAxiosResult.data,
-    });
+    const { sut, mockedAxios } = makeSut();
+    const promise = sut.post(mockPostRequest());
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
   });
 });
