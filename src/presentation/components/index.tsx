15ed09b3853980d84a8717650c4ba03640994e28
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-01-03 01:42:06 -0300

    refactor: move views path to a index file in views folder

--- src/presentation/components/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as Logo } from './Logo';
-export { default as Spinner } from './spinner';
-export { default as LoginHeader } from './login-header';
-export { default as Footer } from './footer';
-export { default as Input } from './input';
-export { default as FormStatus } from './form-status';
