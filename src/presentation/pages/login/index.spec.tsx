a93403de741692b9b8f38272bdf5cb11dd50b916
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-01-04 01:16:55 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react';
 import Login from '.';
 
 describe('LoginView', () => {
-  it('Should not render spinner and error on start', () => {
+  it('Should start with initial state', () => {
     const { getByTestId } = render(<Login />);
     const errorWrapp = getByTestId('error-wrapp');
     expect(errorWrapp.childElementCount).toBe(0);
+    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(true);
   });
 });
