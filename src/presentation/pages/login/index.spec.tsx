d4ab7ff59c7202962af641335dedc9c831ee4c41
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-01-10 14:05:36 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -1,13 +1,30 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 
 import Login from '.';
 
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+  return {
+    sut,
+  };
+};
+
 describe('LoginView', () => {
   it('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />);
-    const errorWrapp = getByTestId('error-wrapp');
+    const { sut } = makeSut();
+    const errorWrapp = sut.getByTestId('error-wrapp');
     expect(errorWrapp.childElementCount).toBe(0);
-    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
+    const emailStatus = sut.getByTestId('email-status');
+    expect(emailStatus.title).toBe('Campo obrigatório');
+    expect(emailStatus.textContent).toBe('⛔');
+    const passwordStatus = sut.getByTestId('password-status');
+    expect(passwordStatus.title).toBe('Campo obrigatório');
+    expect(passwordStatus.textContent).toBe('⛔');
   });
 });
